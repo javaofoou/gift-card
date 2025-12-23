f7ada391ccee8a1234d83d53a1e0ec798f263bc5
javaofoou <almaslahatilislamiyat@gmail.com>
2025-12-23 00:53:53 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 // Optional: fallback to index.html for single-page app
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
-
+});
 // Middleware
 app.use(cors({
   origin: "*",
