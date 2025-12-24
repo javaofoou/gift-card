bef4ebb948676805a21352c44ab677e0b2c24147
javaofoou <almaslahatilislamiyat@gmail.com>
2025-12-24 10:18:08 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const serverless = require("serverless-http");
-const connectDB = require("../lib/db");
 
 const userRoutes = require("../routes/user");
 const adminRoutes = require("../routes/admin");
 
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+app.use(cors());
 app.use(express.json());
 
-// Connect MongoDB
-connectDB(process.env.MONGO_URL);
 app.get("/api/test", (req, res) => {
-  console.log("API hit");
   res.json({ success: true });
 });
-// API Routes
+
 app.use("/api/user", userRoutes);
 app.use("/api/admin", adminRoutes);
 
-// Export for Vercel serverless
 module.exports = serverless(app);
+
